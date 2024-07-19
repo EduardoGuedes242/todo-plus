6ee6eb23d05b042bbb876fb5f6c9bd197eb6a5fd
Eduardo Guedes <eduardoguedesdev@gmail.com>
2024-07-18 22:46:12 -0300

    implementando Validação de login

---
+++ src/main/java/com/guedes/todo_plus_backend/infra/secutiry/TokenService.java
@@ -0,0 +1,13 @@
+package com.guedes.todo_plus_backend.infra.secutiry;
+
+import com.guedes.todo_plus_backend.entity.user.User;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TokenService {
+
+  public String generateToken(User user){
+    return "";
+  }
+
+}
